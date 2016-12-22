32d0ce48933996233f3181af75e81bb342c381ad
parasanger <roger@insouciance.com>
2016-12-22 13:56:47 -0800

    spark1

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+/**
+ *
+ */
+public class Main {
+
+    /**
+     * create API handlers and theads depending
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
